2c646ef111656fa1bc4ab396dc63557f819f1cc7
Manuel <prezcabellomanuel@gmail.com>
2017-07-14 00:11:17 +0000

    Added some routes. These will change

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/create'
+
+  resources :restaurants, only: [:index, :show] do
+    resources :postings, only: [:new, :create, :update, :destroy]
+    resources :reviews, only: [:new, :create, :destroy]
+  end
+
+  resources :workers, only: [:index, :show] do
+    resources :comments, only: [:new, :create, :destroy]
+    resources :reviews, only: [:new, :create, :destroy]
+    resources :skills, only: [:new, :create, :destroy, :update]
+  end
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/login' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
 end
