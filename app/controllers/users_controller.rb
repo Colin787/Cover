1e97b35a7dc8561fe67496d3118011d71f33b74c
Francois <francoisbergeret.francois@gmail.com>
2017-07-16 00:01:05 +0000

    fixed merge issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,5 @@
 class UsersController < ApplicationController
   def new
-<<<<<<< HEAD
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to '/'
-    else
-      # redirect_to '/signup'
-      render :new
-    end
-  end
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-=======
   end
 
   def create
@@ -37,6 +19,5 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:users).permit(:name, :email, :password, :password_confirmation)
->>>>>>> c77762e5c106a3409b0d792b6dfcb618a6e8434e
   end
 end
