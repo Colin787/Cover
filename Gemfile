c94cfeeebd7829613703e1f021095e7f7395736d
Francois <francoisbergeret.francois@gmail.com>
2017-07-18 02:23:20 +0000

    fixed sign up form add bycrypt

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ git_source(:github) do |repo_name|
 end
 
 
+gem 'pry'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.2'
 # Use sqlite3 as the database for Active Record
@@ -13,6 +15,8 @@ gem 'rails', '~> 5.1.2'
 gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
+gem 'bcrypt', '~> 3.1.7'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get 'home/index'
 
   root to: 'home#index'
-  
+
+
+  get '/users/restaurant' => 'users#restaurant'
+  get '/users/worker' => 'users#worker'
   resources :users, except: [:index]
   resources :usertypes, only: [:new, :create, :destroy, :update]
 
@@ -17,6 +20,9 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
+
+  #custom get  user/new/restaurant routes to user/restaurant
 
 end
 
