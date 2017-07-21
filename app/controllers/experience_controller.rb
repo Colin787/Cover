8cfb0686b964a4933f32a6232f1631c716fb9fe2
bickmark <mark.bickford@icloud.com>
2017-07-21 16:43:48 -0700

    Interim commit

--- app/controllers/experience_controller.rb
+++ app/controllers/experience_controller.rb
@@ -1,15 +1,43 @@
 class ExperienceController < ApplicationController
 
+  def index
+    @experience = experience.all
+  end
+
   def new
   end
 
   def create
+    @experience = Experience.new(experience_params)
+    @experience.user = current_user
+    @experience.jobtype = params[:jobtype_id]
 
+    if @experience.save
+      puts "saved experience"
+      redirect_to @experience.user
+    else
+      puts "experience failed"
+      @experience.errors.each {|e|
+        puts e
+      }
+      redirect_to @experience.user
+    end
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   def destroy
   end
 
-  def update
+
+  private
+
+  def experience_params
+    params.require(:experience).permit(:experience)
   end
+
 end
