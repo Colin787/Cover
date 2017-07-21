4d717e4a4b64121617a4f5d7b54dc80aeb3d5de0
Manuel <prezcabellomanuel@gmail.com>
2017-07-21 01:47:45 +0000

    Added google static map feature

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,7 @@
 module JobsHelper
+  def googleMapBuilder(latitude, longitude)
+    "https://maps.googleapis.com/maps/api/staticmap?center=#{latitude}, #{longitude}&zoom=13&size=400x200&maptype=roadmap
+            &markers=color:blue%7C#{latitude}, #{longitude}
+            &key=AIzaSyBT1C6Em-Pe2PoDA4cZla0jUt35fDCFjsg"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717210325) do
+ActiveRecord::Schema.define(version: 20170720234007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 20170717210325) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "usertype_id"
+    t.float "longitude"
+    t.float "latitude"
     t.index ["usertype_id"], name: "index_users_on_usertype_id"
   end
 
