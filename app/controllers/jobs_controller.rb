c4b63badf77dd0a00d2d95041142865a5f6c18f3
Manuel <prezcabellomanuel@gmail.com>
2017-07-21 23:04:35 +0000

    Added jobtype to card. Fixed jobs not rendering bug

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,33 +1,28 @@
 class JobsController < ApplicationController
 
-  #before_action
-
   def new
     if current_user.usertype_id == 2
       redirect_to '/jobs'
     else
     @job = Job.new
-  end
-    # there should be new.html.erb
-    # AND it uses @job
-
-    # it probably will use
-    # form_for(@job) do |form| ...
-
-
+    end
   end
 
   def create
     @job = Job.new(job_params)
+    puts "IM HERE"
     @job.user_id = current_user.id
+    puts "I AM HERE AGAIN"
+    @job.user = current_user
     if @job.save
       redirect_to @job
     else
+      @job.errors.each {|e|
+        puts e
+      }
       redirect_to '/jobs'
     end
   end
-
-
 
   def show
     @job = Job.find params[:id]
@@ -38,9 +33,8 @@ class JobsController < ApplicationController
 
   def index
     @userjobs = Job.find_by user_id: current_user.id
-    @jobs = Job.all          
+    @jobs = Job.all
   end
-
 
   def destroy
     @job = Job.find params[:id]
@@ -61,7 +55,7 @@ class JobsController < ApplicationController
   end
 
   def job_params
-    params.require(:job).permit(:start_time, :end_time, :user_id, :description, :rate, :status)
+    params.require(:job).permit(:start_time, :end_time, :user_id, :description, :rate, :status, :jobtype_id)
   end
 
 end
