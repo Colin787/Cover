7d539ce1cf1cc072b330c517af47eac3b1c087bc
bickmark <mark.bickford@icloud.com>
2017-07-23 14:57:00 -0700

    basic search of /jobs: working

--- app/models/job.rb
+++ app/models/job.rb
@@ -10,4 +10,13 @@ class Job < ApplicationRecord
             :end_time,
             :rate,
             presence: true
+
+  def self.search(search)
+    if search
+      where('cast(rate as text) LIKE ? OR description LIKE ?', "%#{search}%", "%#{search}%")
+    else
+      all
+    end
+  end
+
 end
