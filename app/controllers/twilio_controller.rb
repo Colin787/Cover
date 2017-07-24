f0b7b3edb6b917b7ce7add6e791f9cbd98bfb5a6
Manuel <prezcabellomanuel@gmail.com>
2017-07-24 23:21:51 +0000

    Added twilio

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -4,16 +4,17 @@ require 'twilio-ruby'
 class TwilioController < ApplicationController
   include Webhookable
 
-  after_filter :set_header
+  after_action :set_header
 
   skip_before_action :verify_authenticity_token
 
   def voice
-    response = Twilio::TwiML::Response.new do |r|
-      r.Say 'Hey there. Congrats on integrating Twilio into your Rails 4 app.', :voice => 'alice'
-         r.Play 'http://linode.rabasa.com/cantina.mp3'
+    response = Twilio::TwiML::VoiceResponse.new do |r|
+      r.say 'Hey there. Congrats on integrating Twilio into your Rails 4 app.', :voice => 'alice'
+      # r.Play 'http://linode.rabasa.com/cantina.mp3'
     end
 
-    render_twiml response
+    render xml: response.to_s
+    # render_twiml response
   end
 end
