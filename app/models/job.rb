8e01ac77ea1d18759f6a7c865a970807eca5ec40
Michael Zhu <zlymike@outlook.com>
2017-07-25 03:50:49 +0000

    modified search

--- app/models/job.rb
+++ app/models/job.rb
@@ -10,9 +10,20 @@ class Job < ApplicationRecord
             :rate,
             presence: true
 
+  
+
   def self.search(search)
     if search
-      where('cast(start_time as text) LIKE ? OR cast(end_time as text) LIKE ? OR cast(rate as text) LIKE ? OR description LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+      joins(:jobtype).where('cast(start_time as text) 
+      LIKE ? OR cast(end_time as text) LIKE ?
+       OR cast(rate as text) LIKE ?
+        OR description LIKE ?
+         OR jobtypes.name LIKE ?', 
+        "%#{search}%", 
+        "%#{search}%", 
+        "%#{search}%",
+        "%#{search}%", 
+        "%#{search}%")
     else
       all
     end
