a9572fd1f565512b6a7cabf72b9f7a222fa3991e
bickmark <mark.bickford@icloud.com>
2017-07-26 11:07:21 -0700

    Interim commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,164 +19,136 @@ unless Rails.env.development?
   exit 0
 end
 
+
+## USERTYPES
+
+Usertype.destroy_all
+
+Usertype.find_or_create_by! name: 'restaurant'
+
+Usertype.find_or_create_by! name: 'worker'
+
+
+## USERS
+
+User.destroy_all
+
+User.find_or_create_by!({
+                            email: 'bergeretforestier.francois@gmail.com',
+                            password_digest: '1234',
+                            cell: '7787071254',
+                            first_name: 'Francois',
+                            last_name: 'Bergeret_Forestier',
+                            restaurant_name: '',
+                            street_address: '',
+                            city: '',
+                            province: '',
+                            postal_code: 'P6A 1Z2',
+                            description: '',
+                            image: 'http://lorempixel.com/400/200/people/',
+                        })
+
+User.find_or_create_by!({
+                            email: 'colinpark4@gmail.com',
+                            password_digest: '1234',
+                            cell: '7788884322',
+                            first_name: 'Colin',
+                            last_name: 'Park',
+                            restaurant_name: 'Hawksworth Restaurant',
+                            street_address: '801 West Georgia Street',
+                            city: 'Vancouver',
+                            province: 'BC',
+                            postal_code: 'V6C 1P7',
+                            description: 'Inventive Pacific Northwest cuisine & extensive wine list served in the Rosewood Hotel Georgia.',
+                            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL-A8kRLKY_AxISxj6diUGNrwlLO61o4XwoVXi7ponKr83Xhbe7A',
+                        })
+
+User.find_or_create_by!({
+                            email: 'prezcabellomanuel@gmail.com',
+                            password_digest: '1234',
+                            cell: '6047679548',
+                            first_name: 'Manuel',
+                            last_name: 'Cabello',
+                            restaurant_name: 'Meat & Bread',
+                            street_address: '370 Cambie Street',
+                            city: 'Vancouver',
+                            province: 'BC',
+                            postal_code: 'V6B 1H7',
+                            description: 'We make sandwiches that are high quality, of good value, and prepared with skill as well as heart. The essence of our food simple, fast, delicious is the foundation of Meat & Bread.',
+                            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdtrR8w6mYA9934vUiFgQZwJjRRxICExwmEkpdR-Qb82Smh5Ex',
+                        })
+
+User.find_or_create_by!({
+                            email: 'zlymike@outlook.com',
+                            password_digest: '1234',
+                            cell: '7788620328',
+                            first_name: 'Michael',
+                            last_name: 'Zhu',
+                            restaurant_name: 'Bestie',
+                            street_address: '105 E Pender Street',
+                            city: 'Vancouver',
+                            province: 'BC',
+                            postal_code: 'V6A 1T5',
+                            description: 'German-inspired eatery serving sausages, currywurst, pretzels, beer & wine in cool environs.',
+                            image: 'https://vanfoodies.files.wordpress.com/2014/02/img_20140207_185734.jpg',
+                        })
+
+
+User.find_or_create_by!({
+                            email: 'mark.bickford@icloud.com',
+                            password_digest: '1234',
+                            cell: '7783895004',
+                   3         first_name: 'Mark',
+                            last_name: 'Bickford',
+                            restaurant_name: '',
+                            street_address: '',
+                            city: '',
+                            province: '',
+                            postal_code: 'T2A 1C8',
+                            description: '',
+                            image: 'http://lorempixel.com/400/200/people/',
+                        })
+
+User.find_or_create_by!({
+                            email: 'thespice@hotmail.com',
+                            password_digest: '1234',
+                            cell: '5044054545',
+                            first_name: 'Sean',
+                            last_name: 'Spicer',
+                            restaurant_name: '',
+                            street_address: '',
+                            city: '',
+                            province: '',
+                            postal_code: 'V2P 2M1',
+                            description: '',
+                            image: 'app/assets/images/spicer2.jpg',
+                        })
+
+
 ## JOBTYPES
 
 Jobtype.destroy_all
 
 Jobtype.find_or_create_by! name: 'dishwasher'
 
-Jobtype.find_or_create_by! name: 'line cook'
+Jobtype.find_or_create_by! name: 'prep cook'
+
+Jobtype.find_or_create_by! name: 'line chef'
 
 Jobtype.find_or_create_by! name: 'sous chef'
 
-Jobtype.find_or_create_by! name: 'prep chef'
+Jobtype.find_or_create_by! name: 'busser'
 
-Jobtype.find_or_create_by! name: 'waiter'
+Jobtype.find_or_create_by! name: 'server'
 
-Jobtype.find_or_create_by! name: 'hostess'
+Jobtype.find_or_create_by! name: 'bartender'
 
+Jobtype.find_or_create_by! name: 'host'
 
-## USERTYPES
-
-Usertype.destroy_all
-
-puts "Finding or creating Usertypes..."
-
-typeRestaurant = Usertype.find_or_create_by! name: 'restaurant'
-
-typeWorker = Usertype.find_or_create_by! name: 'worker'
-
-
-## USERS
-
-puts "Finding or creating Users..."
-
-User.destroy_all
-
-userColin = typeRestaurant.users.create!({
-                                             email: 'colinpark4@gmail.com',
-                                             password_digest: '1234',
-                                             cell: '17788884322',
-                                             first_name: 'Colin',
-                                             last_name: 'Park',
-                                             restaurant_name: 'Hawksworth Restaurant',
-                                             street_address: '801 West Georgia Street',
-                                             city: 'Vancouver',
-                                             province: 'BC',
-                                             postal_code: 'V6C 1P7',
-                                             description: 'Inventive Pacific Northwest cuisine & extensive wine list served in the Rosewood Hotel Georgia.',
-                                             image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL-A8kRLKY_AxISxj6diUGNrwlLO61o4XwoVXi7ponKr83Xhbe7A',
-                                         })
-
-userManuel = typeRestaurant.users.create!({
-                                              email: 'prezcabellomanuel@gmail.com',
-                                              password_digest: '1234',
-                                              cell: '16047679548',
-                                              first_name: 'Manuel',
-                                              last_name: 'Cabello',
-                                              restaurant_name: 'Meat & Bread',
-                                              street_address: '370 Cambie Street',
-                                              city: 'Vancouver',
-                                              province: 'BC',
-                                              postal_code: 'V6B 1H7',
-                                              description: 'We make sandwiches that are high quality, of good value, and prepared with skill as well as heart. The essence of our food simple, fast, delicious is the foundation of Meat & Bread.',
-                                              image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdtrR8w6mYA9934vUiFgQZwJjRRxICExwmEkpdR-Qb82Smh5Ex',
-                                          })
-
-userMike = typeRestaurant.users.create!({
-                                            email: 'zlymike@outlook.com',
-                                            password_digest: '1234',
-                                            cell: '17788620328',
-                                            first_name: 'Michael',
-                                            last_name: 'Zhu',
-                                            restaurant_name: 'Bestie',
-                                            street_address: '105 E Pender Street',
-                                            city: 'Vancouver',
-                                            province: 'BC',
-                                            postal_code: 'V6A 1T5',
-                                            description: 'German-inspired eatery serving sausages, currywurst, pretzels, beer & wine in cool environs.',
-                                            image: 'https://vanfoodies.files.wordpress.com/2014/02/img_20140207_185734.jpg',
-                                        })
-
-typeWorker.users.create!({
-                             email: 'bergeretforestier.francois@gmail.com',
-                             password_digest: '1234',
-                             cell: '17787071254',
-                             first_name: 'Francois',
-                             last_name: 'Bergeret_Forestier',
-                             restaurant_name: '',
-                             street_address: '',
-                             city: '',
-                             province: '',
-                             postal_code: 'P6A 1Z2',
-                             description: '',
-                             image: 'http://lorempixel.com/400/200/people/',
-                         })
-
-typeWorker.users.create!({
-                             email: 'mark.bickford@icloud.com',
-                             password_digest: '1234',
-                             cell: '17783895004',
-                             first_name: 'Mark',
-                             last_name: 'Bickford',
-                             restaurant_name: '',
-                             street_address: '',
-                             city: '',
-                             province: '',
-                             postal_code: 'T2A 1C8',
-                             description: '',
-                             image: 'http://lorempixel.com/400/200/people/',
-                         })
-
-typeWorker.users.create!({
-                             email: 'aru@astana.com',
-                             password_digest: '1234',
-                             cell: '15044054545',
-                             first_name: 'Fabio',
-                             last_name: 'Aru',
-                             restaurant_name: '',
-                             street_address: '',
-                             city: '',
-                             province: '',
-                             postal_code: 'N5W 2S9',
-                             description: '',
-                             image: 'http://lorempixel.com/400/200/people/',
-                         })
-
-typeWorker.users.create!({
-                             email: 'christopher@sky.com',
-                             password_digest: '1234',
-                             cell: '15044054545',
-                             first_name: 'Christopher',
-                             last_name: 'Froome',
-                             restaurant_name: '',
-                             street_address: '',
-                             city: '',
-                             province: '',
-                             postal_code: 'V2P 2M1',
-                             description: '',
-                             image: 'http://lorempixel.com/400/200/people/',
-                         })
-
-typeWorker.users.create!({
-                             email: 'bardet@ag2r.com',
-                             password_digest: '1234',
-                             cell: '15044054545',
-                             first_name: 'Romain',
-                             last_name: 'Bardet',
-                             restaurant_name: '',
-                             street_address: '',
-                             city: '',
-                             province: '',
-                             postal_code: 'L9H 1E6',
-                             description: '',
-                             image: 'http://lorempixel.com/400/200/people/',
-                         })
+Jobtype.find_or_create_by! name: 'cashier'
 
 
 ## JOBS
-
-puts "Creates Jobs..."
 
 Job.destroy_all
 
@@ -218,8 +190,6 @@ jobMike1 = userMike.jobs.create!({
 
 ## EXPERIENCES
 
-puts "Creates experiences..."
-
 Experience.destroy_all
 
 Experience.find_or_create_by!({
@@ -230,4 +200,4 @@ Experience.find_or_create_by!({
                                   description: 'Test test test test',
                               })
 
-puts "Done y'all..."
+puts "Finis"
