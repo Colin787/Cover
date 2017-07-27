ed0a336d4681987b501dd8e36dda9732934aa097
Manuel <prezcabellomanuel@gmail.com>
2017-07-27 01:54:22 +0000

    centered location map

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -6,7 +6,7 @@ module JobsHelper
   end
 
   def googleMapBuilderShow(latitude, longitude)
-    "https://maps.googleapis.com/maps/api/staticmap?center=#{latitude}, #{longitude}&zoom=13&size=250x200&maptype=roadmap
+    "https://maps.googleapis.com/maps/api/staticmap?center=#{latitude}, #{longitude}&zoom=13&size=280x200&maptype=roadmap
             &markers=color:blue%7C#{latitude}, #{longitude}
             &key=AIzaSyBT1C6Em-Pe2PoDA4cZla0jUt35fDCFjsg"
   end
